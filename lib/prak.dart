ad72de11b9afdc1fae45adc2461488958d94067e
fahreziahmad <alfahreziahmadsyahyan@gmail.com>
2026-03-07 00:46:24 +0700

    menambahkan background dan gradient pada tampilan halaman

--- lib/prak.dart
+++ lib/prak.dart
@@ -22,4 +22,14 @@ class SimpleStateless extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-     
+      backgroundColor: const Color(0xFFF0F4F8),
+      body: Container(
+        width: double.infinity,
+        decoration: BoxDecoration(
+          gradient: LinearGradient(
+            begin: Alignment.topCenter,
+            end: Alignment.bottomCenter,
+            colors: [Colors.blue.shade50, const Color(0xFFD4E2F1)],
+          ),
+        ),
+       
