e2a7b3438ef9930059af970b1310087b6dabb402
fahreziahmad <alfahreziahmadsyahyan@gmail.com>
2026-03-07 00:39:30 +0700

    menambahkan class CounterManager sebagai StatefulWidget

---
+++ lib/prak.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+
+class CounterManager extends StatefulWidget {
+  const CounterManager({super.key});
+
+  @override
+  State<CounterManager> createState() => _CounterManagerState();
+}
+
